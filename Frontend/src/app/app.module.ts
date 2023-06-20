fd48e4ac47adb7720d046cb2b7b62cc7b50c96a1
Eldinosaur <eldinosaur7u7@outlook.com>
2023-06-20 11:58:51 -0500

    correccion de inicializacion del proyecto

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VentasComponent } from './ventas/ventas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ClientesComponent } from './clientes/clientes.component';
-import { AdministracionComponent } from './administracion/administracion.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    VentasComponent,
-    ClientesComponent,
-    AdministracionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
