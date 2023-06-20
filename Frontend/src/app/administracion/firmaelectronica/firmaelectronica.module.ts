86c00c1ac439d684331e75e666ee86d49ba8695b
Eldinosaur <eldinosaur7u7@outlook.com>
2023-06-20 12:09:35 -0500

    creacion de modulos

---
+++ Frontend/src/app/administracion/firmaelectronica/firmaelectronica.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FirmaelectronicaRoutingModule } from './firmaelectronica-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FirmaelectronicaRoutingModule
+  ]
+})
+export class FirmaelectronicaModule { }
