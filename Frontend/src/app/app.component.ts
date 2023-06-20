c4682ffc4dabac60009ecdafbd4ed329e718b0e3
Eldinosaur <eldinosaur7u7@outlook.com>
2023-06-20 12:59:38 -0500

    creacion de pagina login

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FacturacionElectronica';
+  expanded=true
+  toggleExpanded(expanded:boolean){
+    this.expanded = expanded
+  }
 }
